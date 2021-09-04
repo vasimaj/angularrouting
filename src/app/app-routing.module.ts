77e60771cf507cdca69314ed11828df4436d6058
vasimaj <jamadarvasim88@gmail.com>
2021-09-04 19:22:49 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { HomeComponent} from './home/home.component';
+import { AboutComponent} from './about/about.component';
+import { ContactComponent} from './contact/contact.component';
+import { ServicesComponent} from './services/services.component';
+import { LoginComponent} from './login/login.component';
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes = [
+  {
+  path: 'user',
+  component: UserComponent
+  },
+  {
+  path: 'userdetails',
+  component: UserdetailsComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserdetailsComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
